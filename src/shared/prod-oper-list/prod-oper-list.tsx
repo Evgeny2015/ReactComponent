84adbe9c4df4f9cac558f34ce082da9349bf75ad
evgeny3000 <AlekseevE2015@yandex.ru>
2025-05-16 21:05:15 +0300

    feat: add IntersectionObject, createPortal

---
+++ src/shared/prod-oper-list/prod-oper-list.tsx
@@ -0,0 +1,61 @@
+import React, { FC } from 'react'
+import './prod-oper-list.css'
+import ShortOperation, { ShortOperationProps } from '../short-operation/short-operation'
+import ShortProduct, { ShortProductProps } from '../short-product/short-product'
+
+interface IUniqueItem {
+    id: string
+}
+
+export interface IShortOperationWithId extends ShortOperationProps, IUniqueItem {}
+export interface IShortProductWithId extends ShortProductProps, IUniqueItem {}
+
+interface IOperationList {
+    type: 'operation',
+    data: IShortOperationWithId[];
+}
+
+interface IProductList {
+    type: 'product'
+    data: IShortProductWithId[]
+}
+
+// export type IOperProdWithId = IShortOperationWithId | IShortProductWithId
+type ProdOperListProps = IOperationList | IProductList
+
+/**
+ * Компонент списка товаров/операций в зависимости от выбранного проекта
+ */
+const ProdOperList: FC<ProdOperListProps> = ({ type, data }) => {
+    return (
+        <>
+        {/* <div className={cn('button-container')}> */}
+            { type === 'operation' &&
+                data.map(x => (
+                    <ShortOperation
+                        key={x.id}
+                        category={x.category}
+                        description={x.description}
+                        name={x.name}
+                        sum={x.sum}
+                    />
+                ))
+            }
+
+            { type === 'product' &&
+                data.map(x => (
+                    <ShortProduct
+                        key={x.id}
+                        description={x.description}
+                        image={x.image}
+                        name={x.name}
+                        price={x.price}
+                    />
+                ))
+            }
+        {/* </div> */}
+        </>
+    );
+};
+
+export default ProdOperList;
